737b32a390b88e56f3e85dc9ba2cd079c559c45f
Vaibhav Sharma <100791544+Vaibhav-2611@users.noreply.github.com>
2022-03-02 23:00:15 +0530

    Add files via upload
    Python file with MySQL files are there in the project.

---
+++ Bank Management System.py
@@ -0,0 +1,280 @@
+def clear():
+  for _ in range(65):
+     print()
+
+def customer_record():
+  conn = mysql.connector.connect(
+      host='localhost', database='bankproject', user='root', password="VChintu2611")
+  cursor = conn.cursor()
+  sql ="select * from customer;"
+  cursor.execute(sql)
+  results = cursor.fetchall()
+  print('Customer Records')
+  print('-'*120)
+  for result in results:
+    print(result[0], result[1], result[2], result[3], result[4], result[5],result[6], result[7], result[8])
+  print('-'*120)
+  conn.close()
+  wait = input('\n\n\n Press any key to continue....')
+
+def account_status(acno):
+  conn = mysql.connector.connect(
+      host='localhost', database='bankproject', user='root', password="VChintu2611")
+  cursor = conn.cursor()
+  sql ="select status,balance from customer where acno ='"+acno+"'"
+  result = cursor.execute(sql)
+  result = cursor.fetchone()
+  conn.close()
+  return result
+
+def deposit_amount():
+    conn = mysql.connector.connect(
+        host='localhost', database='bankproject', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    acno = input('Enter account No :')
+    amount = input('Enter amount :')
+    today = date.today()
+    result = account_status(acno)
+    if result [0]== 'active':
+      sql1 ="update customer set balance = balance+"+amount + ' where acno = '+acno+' and status="active";'
+      sql2 = 'insert into transaction(amount,type,acno,dot) values(' + amount +',"deposit",'+acno+',"'+str(today)+'");'
+      cursor.execute(sql2)
+      cursor.execute(sql1)
+      conn.commit()
+      print(sql1)
+      print(sql2)
+      print('\n\namount deposited')
+    else:
+      print('\n\nClosed or Suspended Account....')    
+    wait= input('\n\n\n Press any key to continue....')
+    conn.close()
+
+def withdraw_amount():
+    conn = mysql.connector.connect(
+        host='localhost', database='bankproject', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    acno = input('Enter account No :')
+    amount = input('Enter amount :')
+    today = date.today()
+    result = account_status(acno)
+    if result[0] == 'active' and int(result[1])>=int(amount):
+      sql1 = "update customer set balance = balance-" + \
+          amount + ' where acno = '+acno+' and status="active";'
+      sql2 = 'insert into transaction(amount,type,acno,dot) values(' + \
+          amount + ',"withdraw",'+acno+',"'+str(today)+'");'
+      cursor.execute(sql2)
+      cursor.execute(sql1)
+      conn.commit()
+      print(sql1)
+      print(sql2)
+      print('\n\namount Withdrawn')
+    else:
+      print('\n\nClosed or Suspended Account.Or Insufficient amount')
+    wait = input('\n\n\n Press any key to continue....')
+    conn.close()
+def transaction_menu():
+    while True:
+      print(' Trasaction Menu')
+      print("\n1.  Deposit Amount")
+      print('\n2.  WithDraw Amount')
+      print('\n3.  Back to Main Menu')
+      print('\n\n')
+      choice = int(input('Enter your choice ...: '))
+      if choice == 1:
+        deposit_amount()
+      if choice == 2:
+        withdraw_amount()
+      if choice == 3:
+        break
+
+def search_menu():
+    conn = mysql.connector.connect(
+       host='localhost', database='bankproject', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    while True:
+      print(' Search Menu')
+      print("\n1.  Account No")
+      print('\n2.  Aadhar Card')
+      print('\n3.  Phone No')
+      print('\n4.  email')
+      print('\n5.  Names')
+      print('\n6.  Back to Main Menu')
+      print('\n\n')
+      choice = int(input('Enter your choice ...: '))
+      field_name=''
+      if choice == 1:
+        field_name ='acno'
+      if choice == 2:
+        field_name ='aadhar_no'
+      if choice == 3:
+        field_name = 'phone'
+      if choice == 4:
+        field_name = 'email'
+      if choice == 5:
+        field_name = 'name'
+      if choice == 6:
+        break
+      msg ='Enter '+field_name+': '
+      value = input(msg)
+      if field_name=='acno':
+        sql = 'select * from customer where '+field_name + ' = '+value+';'
+      else:
+        sql = 'select * from customer where '+field_name +' like "%'+value+'%";'
+      #print(sql)
+      cursor.execute(sql)
+      records = cursor.fetchall()
+      n = len(records)
+      print('Search Result for ', field_name, ' ',value)
+      print('-'*80)
+      for record in records:
+       print(record[0], record[1], record[2], record[3],
+             record[4], record[5], record[6], record[7], record[8])
+      if(n <= 0):
+        print(field_name, ' ', value, ' does not exist')
+      wait = input('\n\n\n Press any key to continue....')
+    conn.close()
+    wait=input('\n\n\n Press any key to continue....')
+
+def account_details():
+    acno = input('Enter account no :')
+    conn = mysql.connector.connect(
+        host='localhost', database='bankproject', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    sql ='select * from customer where acno ='+acno+';'
+    sql1 = 'select tid,dot,amount,type from transaction t where t.acno='+acno+';'
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    print('Account Details')
+    print('-'*120)
+    print('Account No :',result[0])
+    print('Customer Name :',result[1])
+    print('Address :',result[2])
+    print('Phone NO :',result[3])
+    print('email ID :',result[4])
+    print('Aadhar No :',result[5])
+    print('Account Type :',result[6])
+    print('Account Status :',result[7])
+    print('Current Balance :',result[8])
+    print('-'*120)
+    cursor.execute(sql1)
+    results = cursor.fetchall()
+    for result in results:
+        print(result[0], result[1], result[2], result[3])
+    conn.close()
+    wait=input('\n\n\nPress any key to continue.....')
+
+def report_menu():
+    while True:
+      print(' Report Menu')
+      print('\n1.  Account Details')
+      print('\n2.  Customer Records')
+      print('\n3.  Back to Main Menu')
+      print('\n\n')
+      choice = int(input('Enter your choice ...: '))
+      if choice == 1:
+        account_details()
+      if choice == 2:
+        customer_record()
+      if choice == 3:
+        break
+
+def add_account():
+    conn = mysql.connector.connect(
+        host='localhost', database='bankproject', user='root', password="VChintu2611")
+    cursor = conn.cursor()   
+    name = input('Enter Name :')
+    addr = input('Enter address ')
+    phone = input('Enter Phone no :')
+    email = input('Enter email :')
+    aadhar = input('Enter Aadhar no :')
+    actype = input('Account Type (saving/current ) :')
+    balance = input('Enter opening balance :')
+    sql = 'insert into customer(name,address,phone,email,aadhar_no,acc_type,balance) values ("' + name +'","'+ addr+'","'+phone+'","'+email+'",\
+                                                                                                                                                                                                "'+aadhar+'","'+actype+'",'+balance+');'
+    #print(sql)
+    cursor.execute(sql)
+    conn.commit()
+    conn.close()
+    print('\n\nNew customer added successfully')
+    wait= input('\n\n\n Press any key to continue....')
+
+def modify_account():
+    conn = mysql.connector.connect(
+        host='localhost', database='bankproject', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    acno = input('Enter customer Account No :')
+    print('Modify screen ')
+    print('\n 1.  Customer Name')
+    print('\n 2.  Customer Address')
+    print('\n 3.  Customer Phone No')
+    print('\n 4.  Customer email ID')
+    choice = int(input('What do you want to change ? '))
+    new_data  = input('Enter New value :')
+    field_name=''
+    if choice == 1:
+       field_name ='name'
+    if choice == 2:
+       field_name = 'address'
+    if choice == 3:
+       field_name = 'phone'
+    if choice == 4:
+       field_name = 'email'
+    sql ='update customer set ' + field_name + '="'+ new_data +'" where acno='+acno+';' 
+    #print(sql)
+    cursor.execute(sql)
+    conn.commit()
+    print('\n\nCustomer Information modified..')
+    wait = input('\n\n\n Press any key to continue....')
+
+def close_account():
+    conn = mysql.connector.connect(
+        host='localhost', database='bankproject', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    acno = input('Enter customer Account No :')
+    sql ='update customer set status="close" where acno ='+acno+';'
+    cursor.execute(sql)
+    print('\n\nAccount closed')
+    wait = input('\n\n\n Press any key to continue....')
+
+def activate_account():
+    conn = mysql.connector.connect(
+        host='localhost', database='bankproject', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    acno = input('Enter customer Account No :')
+    sql = 'update customer set status="active" where acno ='+acno+';'
+    cursor.execute(sql)
+    print('\n\nAccount Activated')
+    wait = input('\n\n\n Press any key to continue....')
+
+def main_menu():
+    while True:
+      print(' Main Menu')
+      print("\n1.  Add Account")
+      print('\n2.  Modify Account')
+      print('\n3.  Close Account')
+      print('\n4.  Activate Account')
+      print('\n5.  Transaction Menu')
+      print('\n6.  Search Menu')
+      print('\n7.  Report Menu')
+      print('\n8.  Close application')
+      print('\n\n')
+      choice = int(input('Enter your choice ...: '))
+      if choice == 1:
+        add_account()
+      if choice == 2:
+        modify_account()
+      if choice == 3:
+        close_account()
+      if choice == 4:
+        activate_account()
+      if choice ==5 :
+        transaction_menu()
+      if choice ==6 :
+        search_menu()
+      if choice == 7:
+        report_menu()
+      if choice ==8:
+        break
+
+if __name__ == "__main__":
+    main_menu()
---
+++ Stock Management System.py
@@ -0,0 +1,235 @@
+#   project name    : Grocery Stock Management System
+#   made by         : Vaibhav Sharma
+#   session         : 2020-2021
+
+import mysql.connector
+from datetime import date
+from prettytable import PrettyTable
+
+def clear():
+  for _ in range(65):
+     print()
+
+def item_status(idr):
+    conn = mysql.connector.connect(
+        host='localhost', database='stock', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    sql = 'select * from items where id ='+idr
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    return result[3]
+
+def add_item():
+    conn = mysql.connector.connect(
+        host='localhost', database='stock', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    name = input('Enter Item Name :')
+    qty = input('Enter Quantity : ')
+    price = input('Enter Price :')
+    today = date.today()
+    reorder = input('Enter Reorder Level :')
+    sql = 'insert into items(name,price,qty,reorder) values ( "' + \
+        name + '",' + price+','+qty+','+reorder+' );'
+    sql2 = 'insert into transaction(dot,qty,type) values ("'+str(today)+'",'+qty+',"purchase");'
+    #print(sql)
+    cursor.execute(sql)
+    cursor.execute(sql2)
+    conn.commit()
+    conn.close()
+    print('\n\nNew Item added successfully')
+    wait = input('\n\n\n Press any key to continue....')
+
+def sell_item():
+    conn = mysql.connector.connect(
+        host='localhost', database='stock', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    today = date.today()
+    item_no = input('Enter Item ID :')
+    qty = input('Enter sold Quantity : ')
+    sql = 'update items set qty = qty-'+ qty +' where id='+item_no+';'
+    sql2 = 'insert into transaction(dot,qty,type) values ("' + \
+        str(today)+'",'+qty+',"sold");'
+    #print(sql)
+    cursor.execute(sql)
+    cursor.execute(sql2)
+    print('\n\nItem upated successfully')
+    conn.commit()
+    conn.close()    
+    wait = input('\n\n\n Press any key to continue....')
+
+def received_item():
+    conn = mysql.connector.connect(
+        host='localhost', database='stock', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    today = date.today()
+    item_no = input('Enter Item ID :')
+    qty = input('Enter sold Quantity : ')
+    sql = 'update items set qty = qty+' + qty + ' where id='+item_no+';'
+    sql2 = 'insert into transaction(dot,qty,type) values ("' + \
+        str(today)+'",'+qty+',"purchase");'
+    #print(sql)
+    cursor.execute(sql)
+    cursor.execute(sql2)
+    conn.close()
+    print('\n\nItem upated successfully')
+    conn.commit()
+    wait = input('\n\n\n Press any key to continue....')
+
+def search_item():
+    conn = mysql.connector.connect(
+        host='localhost', database='stock', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    sql ="select * from items;"
+    cursor.execute(sql)
+    records = cursor.fetchall()
+    for record in records:
+        print(record)
+    conn.close()
+    print('-'*120)
+    wait = input('\n\n\n Press any key to continue....')
+
+def all_item_list():
+    while True:
+      print('All Item List')
+      print("\n1.  All Item List")
+      print('\n2.  back to Main Menu')
+      print('\n\n')
+      choice = int(input('Enter your choice ...: '))
+      if choice == 1:
+        search_item()
+      if choice == 2:
+        break
+
+def report_items():
+    conn = mysql.connector.connect(
+        host='localhost', database='stock', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    name = input('Enter Item Name :')
+    sql = "select * from items;"
+    cursor.execute(sql)
+    records = cursor.fetchall()
+    print('Item List')
+    t = PrettyTable(['ID', 'Item Name', 'Price', 'Quantity', 'Reorder Level'])
+    for idr, name, price, qty, reorder in records:
+      t.add_row([idr,name,price,qty,reorder])
+    # print(idr,name,fname,add,phone,email)
+    print(t)
+    conn.close()
+    wait = input('\n\n\n Press any key to continue....')
+
+def report_qty_greater_reorder():
+    conn = mysql.connector.connect(
+        host='localhost', database='stock', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    sql = "select * from items where qty > reorder;"
+    cursor.execute(sql)
+    records = cursor.fetchall()
+    print('Item List')
+    t = PrettyTable(['ID', 'Item Name', 'Price', 'Quantity', 'Reorder Level'])
+    for idr, name, price, qty, reorder in records:
+      t.add_row([idr, name, price, qty, reorder])
+    # print(idr,name,fname,add,phone,email)
+    print(t)
+    conn.close()
+    wait = input('\n\n\n Press any key to continue....')
+
+def report_qty_lesser_reorder():
+    conn = mysql.connector.connect(
+        host='localhost', database='stock', user='root', password="VChintu2611")
+    cursor = conn.cursor()
+    sql = "select * from items where qty < reorder;" 
+    cursor.execute(sql) 
+    records = cursor.fetchall() 
+    print('Item List') 
+    t = PrettyTable(['ID', 'Item Name', 'Price', 'Quantity', 'Reorder Level']) 
+    for idr, name, price, qty, reorder in records:
+           t.add_row([idr, name, price, qty, reorder]) 
+           # print(idr,name,fname,add,phone,email) 
+    print(t) 
+    conn.close() 
+    wait = input('\n\n\n Press any key to continue....') 
+
+def report_sold_items(): 
+      conn = mysql.connector.connect( host='localhost', database='stock', user='root', password="VChintu2611") 
+      cursor = conn.cursor() 
+      today = date.today() 
+      sql = "select id name,dot,t.qty,price from items,transaction t where id=tid and type='sold' and dot='"+str(today)+"';" 
+      cursor.execute(sql)
+      records = cursor.fetchall() 
+      t = PrettyTable(['ID', 'Item Name', 'DOT', 'Quantity', 'Price'])
+      for idr, name, dot, qty, price in records: 
+         t.add_row([idr, name, dot,qty,price]) 
+         # print(idr,name,fname,add,phone,email) 
+      print('Items Sold Today :', today) 
+      print(t)
+      conn.close() 
+      wait = input('\n\n\n Press any key to continue....') 
+
+def report_received_item(): 
+      conn = mysql.connector.connect( host='localhost', database='stock', user='root', password="VChintu2611") 
+      cursor = conn.cursor() 
+      today = date.today() 
+      sql = "select id, name,dot,t.qty,price from items,transaction t where id=tid and type='purchase' and dot='" + \
+             str(today)+"';" 
+      cursor.execute(sql)
+      records = cursor.fetchall() 
+      t = PrettyTable(['ID', 'Item Name', 'DOT', 'Quantity', 'Price'])
+      for idr, name, dot, qty, price in records:
+          t.add_row([idr, name, dot, qty, price])
+          # print(idr,name,fname,add,phone,email)  
+      print('Items Received Today :',today) 
+      print(t)
+      conn.close() 
+      wait = input('\n\n\n Press any key to continue....') 
+
+def report_menu(): 
+  while True:  
+      print(' Report Menu') 
+      print("\n1. Item List") 
+      print('\n2. sold Items - Today') 
+      print('\n3. Received Items- Today') 
+      print('\n4. Item Qty > Reorder')
+      print('\n5.  Item Qty < Reorder')
+      print('\n6.  Exit to main Menu')
+      print('\n\n')
+      choice = int(input('Enter your choice ...: '))
+      if choice == 1:
+        report_items()
+      if choice == 2:
+        report_sold_items()
+      if choice == 3:
+        report_received_item()
+      if choice == 4:
+        report_qty_greater_reorder()
+      if choice == 5:
+        report_qty_lesser_reorder()
+      if choice == 6:
+        break
+
+def main_menu():
+    while True:
+      print(' Main Menu')
+      print("\n1.  Add Items")
+      print('\n2.  sell Items')
+      print('\n3.  Received Items')
+      print('\n4.  Item Lists')
+      print('\n5.  Report Menu')
+      print('\n6.  Close application')
+      print('\n\n')
+      choice = int(input('Enter your choice ...: '))
+      
+      if choice == 1:
+        add_item()
+      if choice == 2:
+        sell_item()
+      if choice == 3:
+        received_item()
+      if choice == 4:
+        search_item()
+      if choice == 5:
+        report_menu()
+      if choice == 6:
+        break
+
+if __name__ == "__main__":
+    main_menu()
